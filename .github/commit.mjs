674e2853ca450b3a5a1c24120a40cba0dbaf4238
leonid-shutov-itechart <leonid.shutov@itechart-group.com>
2023-04-26 21:28:13 +0300

    feat: script to commit files to github

---
+++ .github/commit.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env zx
+
+await $`git config --local user.email "itag.labs.automation@itag-labs.com"`;
+await $`git config --local user.name "itag-labs-automation"`;
+await $`git add ."`;
+await $`git commit -m "${argv.message || "commit all files"}"`;
+await $`git push"`;
