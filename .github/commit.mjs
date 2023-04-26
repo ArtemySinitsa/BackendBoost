41bd5abd5229937c6539d302ff1ce426f886d06d
leonid-shutov-itechart <leonid.shutov@itechart-group.com>
2023-04-26 21:32:20 +0300

    fix: typo

--- .github/commit.mjs
+++ .github/commit.mjs
@@ -2,6 +2,6 @@
 
 await $`git config --local user.email "itag.labs.automation@itag-labs.com"`;
 await $`git config --local user.name "itag-labs-automation"`;
-await $`git add ."`;
+await $`git add .`;
 await $`git commit -m "${argv.message || "commit all files"}"`;
 await $`git push"`;
