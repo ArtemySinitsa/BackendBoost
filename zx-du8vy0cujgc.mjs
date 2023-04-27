2495db0a9d5132f1446dc5660b6de6613ccdae86
leonid-shutov-itechart <leonid.shutov@itechart-group.com>
2023-04-27 23:30:38 +0300

    feat: extract zx script to a file

--- zx-du8vy0cujgc.mjs
+++
@@ -1,9 +0,0 @@
-if (await $`git diff skills`) {
-  echo('Changes in the skills folder detected');
-  echo('Building roadmaps...');
-  await $`npx zx build/build.mjs`;
-  echo('Commiting roadmaps...');
-  await $`npx zx .github/commit.mjs --message "build roadmaps"`;
-} else {
-  echo('No changes in the skills folder');
-}
