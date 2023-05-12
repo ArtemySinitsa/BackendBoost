21292c79167614f9a6d57847191b63a648aa9733
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2023-05-12 14:39:43 +0300

    get out of jail

--- .github/commit.mjs
+++ .github/commit.mjs
@@ -2,8 +2,8 @@
 
 const DEFAULT_COMMIT_MESSAGE = "commit all files";
 
-await $`git config --local user.email "itag.labs.automation@itag-labs.com"`;
-await $`git config --local user.name "itag-labs-automation"`;
+await $`git config --local user.email "backendboost@automation.com"`;
+await $`git config --local user.name "backendboost"`;
 await $`git add .`;
 await $`git commit -m "${argv.message || DEFAULT_COMMIT_MESSAGE}"`;
 await $`git push`;
