09c6bceb065037e535d23f4af7623eee83f6a634
leonid-shutov <leonid.shutov.main@gmail.com>
2023-05-26 12:54:00 +0300

    feat: improve update script

--- .github/workflows/update-books.mjs
+++ .github/workflows/update-books.mjs
@@ -2,14 +2,40 @@
 
 $.verbose = false;
 
-if (await $`git diff --name-only HEAD~ -- skills`.then(({ stdout }) => stdout)) {
-  echo("Changes in the skills folder detected");
+const changesToCheck = ["skills", "build/config.json"];
+
+const getOutput = ({ stdout }) => stdout;
+
+const getDiff = (resource) => $`git diff --name-only HEAD~ -- ${resource}`.then(getOutput);
+
+const getWorkingTreeStatus = () => $`git status --porcelain`.then(getOutput);
+
+const buildBooks = () => $`npm run build`;
+
+const commit = (message) => $`npx zx .github/commit.mjs --message "${message}"`;
+
+const updateBooks = async () => {
+  const diffs = await Promise.all(changesToCheck.map((resource) => getDiff(resource)));
+  const changes = diffs.filter(Boolean);
+
+  if (changes.length === 0) {
+    changesToCheck.forEach((resource) => echo(`No changes in ${resource}`));
+    echo("Nothing to rebuild");
+    process.exit(0);
+  }
+
+  echo("Changes detected");
 
   echo("Building books...");
-  await $`npm run build`;
+  await buildBooks();
+
+  if (!(await getWorkingTreeStatus())) {
+    echo("Nothing to commit");
+    process.exit(0);
+  }
 
   echo("Commiting books...");
-  await $`npx zx .github/commit.mjs --message "build books"`;
-} else {
-  echo("No changes in the skills folder detected");
-}
+  await commit("build books");
+};
+
+updateBooks();
