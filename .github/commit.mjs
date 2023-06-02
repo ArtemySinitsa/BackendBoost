eec29b83c381e2e5b215396e2bd2686e2aa6f787
leonid-shutov <leonid.shutov.main@gmail.com>
2023-06-02 20:29:50 +0300

    test: commit message quotes

--- .github/commit.mjs
+++ .github/commit.mjs
@@ -5,5 +5,5 @@ const DEFAULT_COMMIT_MESSAGE = "commit all files";
 await $`git config --local user.email "backendboost@automation.com"`;
 await $`git config --local user.name "backendboost"`;
 await $`git add .`;
-await $`git commit -m "${argv.message || DEFAULT_COMMIT_MESSAGE}"`;
+await $`git commit -m ${'"' + argv.message || DEFAULT_COMMIT_MESSAGE + '"'}`;
 await $`git push`;
