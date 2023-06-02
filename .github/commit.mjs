58577def37cc3a28414e6f0f5a516e8e7ee75de4
leonid-shutov <leonid.shutov.main@gmail.com>
2023-06-02 20:33:46 +0300

    Merge branch 'feat/repository-structure' of ssh://github.com-leonid-shutov-itechart/ArtemySinitsa/BackendBoost into feat/repository-structure

--- .github/commit.mjs
+++ .github/commit.mjs
@@ -5,5 +5,6 @@ const DEFAULT_COMMIT_MESSAGE = "commit all files";
 await $`git config --local user.email "backendboost@automation.com"`;
 await $`git config --local user.name "backendboost"`;
 await $`git add .`;
-await $`git commit -m ${'"' + argv.message || DEFAULT_COMMIT_MESSAGE + '"'}`;
+const message = `"${argv.message || DEFAULT_COMMIT_MESSAGE}"`;
+await $`git commit -m ${message}`;
 await $`git push`;
